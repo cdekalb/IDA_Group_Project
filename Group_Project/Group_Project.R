a6ad781eeb0816ad21c69b2641465fb2a6699d53
Kevin Oberlag <koberlag@gmail.com>
2019-12-07 21:57:39 -0600

    Added code for player and created several models. Updated confusion matrix to use test instead of train data.

--- Group_Project/Group_Project.R
+++ Group_Project/Group_Project.R
@@ -23,7 +23,6 @@ teams <- read_csv("teams.csv")
 
 # read in players data
 players <- read_csv("players.csv") 
-player_team <- players %>% left_join(teams, by=c("Tm","Season"))
 
 head(teams)
 
@@ -47,6 +46,28 @@ teams$PFperG <- teams$PF / teams$G
 
 head(teams)
 
+# Filter out observations for 'Total' Stats
+players <- players %>% filter(Tm != "TOT")
+
+# Check missingness
+players %>% mutate_all(is.na) %>% summarise_all(mean) %>% glimpse
+
+# Impute 0 for percentage fields that had 0 attempts
+players<-players %>% replace_na(list(`FG%` = 0, `2P%` = 0, `3P%` = 0, `eFG%` = 0, `FT%` = 0, `TS%` = 0))
+
+# Aggregate player data per team and season
+players_aggr <- players %>%
+  group_by(Tm, Season) %>%
+  summarise(
+    Age_mean = mean(Age),
+    WS_mean = mean(WS),
+    BPM_mean = mean(BPM),
+    VORP_mean = mean(VORP),
+    priorYearAllNBA_sum = sum(priorYearAllNBA)
+  )
+
+teams <- teams %>% inner_join(players_aggr, by=c("Tm","Season"))
+
 # select 70% of the teams data to form our training data and the remaining 30% to use as test data
 noTrainRows <- floor(nrow(teams) * .7)
 set.seed(4)
@@ -56,38 +77,38 @@ testData <- teams[-sampleRows,]
 
 # Plot exploration
 
-ggplot(data = teams, aes(x = teams$Finals, y = teams$`FG%`)) + geom_boxplot() + labs(x = "Finals", y = "FG%")
-# FG% appears to be a decent predictor of Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$`2P%`)) + geom_boxplot() + labs(x = "Finals", y = "2P%")
-# 2P% appears to be a decent predictor of Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$`3P%`)) + geom_boxplot() + labs(x = "Finals", y = "3P%")
-# 3P% appears to have little to no correlation with Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$`FT%`)) + geom_boxplot() + labs(x = "Finals", y = "FT%")
-# FT% appears to have little to no correlation with Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$TRBperG)) + geom_boxplot() + labs(x = "Finals", y = "TRBperG")
-# TRBperG appears to be a decent predictor of Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$ASTperG)) + geom_boxplot() + labs(x = "Finals", y = "ASLperG")
-# ASTperG appears to be a decent predictor of Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$STLperG)) + geom_boxplot() + labs(x = "Finals", y = "STLperG")
-# STLperG appears to have minimal correlation with Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$BLKperG)) + geom_boxplot() + labs(x = "Finals", y = "BLKperG")
-# BLKperG appears to have minimal correlation with Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$PTSperG)) + geom_boxplot() + labs(x = "Finals", y = "PTSperG")
-# PTSperG appears to have minimal correlation with Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$PFperG)) + geom_boxplot() + labs(x = "Finals", y = "PFperG")
-# PFperG appears to have minimal correlation with Finals
-
-ggplot(data = teams, aes(x = teams$Finals, y = teams$`W/L%`)) + geom_boxplot() + labs(x = "Finals", y = "W/L%")
-# W/L% appears to be a very good predictor of Finals
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$`FG%`)) + geom_boxplot() + labs(x = "Finals", y = "FG%")
+# # FG% appears to be a decent predictor of Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$`2P%`)) + geom_boxplot() + labs(x = "Finals", y = "2P%")
+# # 2P% appears to be a decent predictor of Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$`3P%`)) + geom_boxplot() + labs(x = "Finals", y = "3P%")
+# # 3P% appears to have little to no correlation with Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$`FT%`)) + geom_boxplot() + labs(x = "Finals", y = "FT%")
+# # FT% appears to have little to no correlation with Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$TRBperG)) + geom_boxplot() + labs(x = "Finals", y = "TRBperG")
+# # TRBperG appears to be a decent predictor of Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$ASTperG)) + geom_boxplot() + labs(x = "Finals", y = "ASLperG")
+# # ASTperG appears to be a decent predictor of Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$STLperG)) + geom_boxplot() + labs(x = "Finals", y = "STLperG")
+# # STLperG appears to have minimal correlation with Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$BLKperG)) + geom_boxplot() + labs(x = "Finals", y = "BLKperG")
+# # BLKperG appears to have minimal correlation with Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$PTSperG)) + geom_boxplot() + labs(x = "Finals", y = "PTSperG")
+# # PTSperG appears to have minimal correlation with Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$PFperG)) + geom_boxplot() + labs(x = "Finals", y = "PFperG")
+# # PFperG appears to have minimal correlation with Finals
+# 
+# ggplot(data = teams, aes(x = teams$Finals, y = teams$`W/L%`)) + geom_boxplot() + labs(x = "Finals", y = "W/L%")
+# # W/L% appears to be a very good predictor of Finals
 
 # Description of modeling approach
 
@@ -106,8 +127,13 @@ control = trainControl(method = "repeatedcv",
                        summaryFunction=mnLogLoss, 
                        verboseIter = TRUE)
 
-glm_fit = train(Finals ~ `2P%` + `3P%` + `W/L%` + PFperG, 
-                data= trainData, 
+
+################################################################################
+# glm Model
+################################################################################
+glm_fit = train(Finals ~ `2P%` + `3P%` + `W/L%` + PFperG + Age_mean + WS_mean +
+                  BPM_mean + VORP_mean + priorYearAllNBA_sum
+                ,data= trainData, 
                 method = "glm", 
                 family = binomial(), 
                 metric = "logLoss", 
@@ -116,25 +142,127 @@ glm_fit = train(Finals ~ `2P%` + `3P%` + `W/L%` + PFperG,
 
 summary(glm_fit)
 
-glm_predictions <-predict(object=glm_fit, newdata = testData, type="prob")[,2]
+# glm_predictions <-predict(object=glm_fit, newdata = testData, type="prob")[,2]
+# 
+# # probabilities
+# glm_probs<-tibble(Team=testData$Tm, Season=testData$Season, predProbOfFinalsBerth=glm_predictions, predFinalsBerth=glm_predictions>0.5)
 
-glmSubmission<-tibble(Team=testData$Tm, Season=testData$Season, predProbOfFinalsBerth=glm_predictions, predFinalsBerth=glm_predictions>0.5)
+
+################################################################################
+# MARS Model
+################################################################################
+
+mars_fit = train(Finals ~ `2P%` + `3P%` + `W/L%` + PFperG +
+                  BPM_mean + priorYearAllNBA_sum
+                ,data= trainData,
+                method = "earth",
+                # family = binomial(),
+                metric = "logLoss",
+                glm=list(family=binomial),
+                trControl = control,
+                preProcess = c("center", "scale")) #important to set metric to logLoss to tune for logloss minimization
+
+summary(mars_fit)
+
+# mars_predictions <-predict(object=mars_fit, newdata = testData, type="prob")[,2]
+# 
+# # probabilities
+# mars_probs<-tibble(Team=testData$Tm, Season=testData$Season, predProbOfFinalsBerth=mars_predictions, predFinalsBerth=mars_predictions>0.5)
+# 
+
+################################################################################
+# ctree Model
+################################################################################
+
+dtree_fit = train(Finals ~ `2P%` + `3P%` + `W/L%` + PFperG +
+                  BPM_mean + priorYearAllNBA_sum
+                ,data= trainData,
+                method = "ctree",
+                # family = binomial(),
+                metric = "logLoss",
+                trControl = control,
+                preProcess = c("center", "scale")) #important to set metric to logLoss to tune for logloss minimization
+
+summary(dtree_fit)
+
+################################################################################
+# rf Model
+################################################################################
+
+rf_fit = train(Finals ~ `2P%` + `3P%` + `W/L%` + PFperG +
+                  BPM_mean + priorYearAllNBA_sum
+                ,data= trainData,
+                method = "rf",
+                # family = binomial(),
+                metric = "logLoss",
+                # glm=list(family=binomial),
+                # tuneLength=5,
+                trControl = control,
+                preProcess = c("center", "scale")) #important to set metric to logLoss to tune for logloss minimization
+
+summary(rf_fit)
+
+################################################################################
+# svm Model
+################################################################################
+
+svm_fit = train(Finals ~ `2P%` + `3P%` + `W/L%` + PFperG +
+                 BPM_mean + priorYearAllNBA_sum
+               ,data= trainData,
+               method = "svmRadial",
+               # family = binomial(),
+               metric = "logLoss",
+               # glm=list(family=binomial),
+               # tuneLength=5,
+               trControl = control,
+               preProcess = c("center", "scale")) #important to set metric to logLoss to tune for logloss minimization
+
+summary(svm_fit)
+
+################################################################################
+# xgb Model
+################################################################################
+
+xgb_fit = train(Finals ~ `2P%` + `3P%` + `W/L%` + PFperG +
+                  BPM_mean + priorYearAllNBA_sum
+                ,data= trainData,
+                method = "xgbDART",
+                # family = binomial(),
+                metric = "logLoss",
+                # glm=list(family=binomial),
+                tuneLength=5,
+                trControl = control,
+                preProcess = c("center", "scale")) #important to set metric to logLoss to tune for logloss minimization
+
+summary(xgb_fit)
 
 
-pred <- as.numeric(glm_fit$finalModel$fitted.values>0.5)
 
-teams$Finals <- ifelse(teams$Finals == "Yes", 1, 0)
-trainData <- teams[sampleRows,]
-testData <- teams[-sampleRows,]
+ #  #predict xgb and select the 'yes' predicitions
+ # xgb_predictions <- predict(object = xgb_fit, newdata = testData, type="prob")[,2]
+ #  
+  # # probabilities
+  # xgb_probs<-tibble(Team=testData$Tm, Season=testData$Season, predProbOfFinalsBerth=xgb_predictions, predFinalsBerth=xgb_predictions>0.5)
 
-actual <- as.numeric(trainData$Finals)
 
+################################################################################
+# Model predictions for test
+################################################################################
+
+# Set which model to use for predictions and calculation of confusion matrix
+### NOTE: Change the model for the object = '' variable, to choose the model to compare 
+model_predictions = predict(object = glm_fit, newdata = testData, type="prob")[,2]
+
+# compare predictions to test
+actual <- ifelse(testData$Finals == "Yes", 1, 0)
+  
+pred <- as.numeric(model_predictions>0.5)
 
 m <- confusionMatrix(as.factor(pred), as.factor(actual), positive="1")
 
 m
 
-# Initial results
+
 
 
 
